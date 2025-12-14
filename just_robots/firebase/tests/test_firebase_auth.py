467052c3b507bafce142adbc66d2bef5ee534349
Swapnil Patel <swapnilpatel95@gmail.com>
2025-12-14 10:05:09 -0500

    remove firebase sub-package because its now in a separate repo and included as a submodule

--- just_robots/firebase/tests/test_firebase_auth.py
+++
@@ -1,9 +0,0 @@
-from just_robots.firebase.firebase_auth_config import FirebaseAuthConfig
-from just_robots.firebase.firebase_auth_server import (
-    initialize_firebase_auth,
-    verify_firebase_token,
-)
-
-
-def test_firebase_admin_auth_basic():
-    pass
