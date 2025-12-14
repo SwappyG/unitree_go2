9181dc9ef26c17f2a09c1551c191a32f5425e257
Swapnil Patel <swapnilpatel95@gmail.com>
2025-12-14 10:09:23 -0500

    remove obsolete arcana_go2 folder

--- just_robots/arcana_go2/json_utils.py
+++
@@ -1,6 +0,0 @@
-from __future__ import annotations
-import typing as t
-
-JSONKey: t.TypeAlias = str
-JSONValue: t.TypeAlias = "None | bool | int | float | str | list[JSONValue] | dict[str, JSONValue]"
-JSONObject: t.TypeAlias = "dict[JSONKey, JSONValue]"
